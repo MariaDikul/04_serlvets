61b6028bba8e6cb4babd69c8f331507338b3509f
Maria Dikul <dikul.m@mail.ru>
2023-07-13 23:28:02 +0300

    Initial commit

---
+++ servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,46 @@
+package ru.netology.repository;
+
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+// Stub
+public class PostRepository {
+    private ConcurrentHashMap<Long, Post> repository = new ConcurrentHashMap<>();
+    private AtomicLong counter = new AtomicLong(1);
+    public List<Post> all() {
+        List<Post> list = new ArrayList<>();
+        for (Map.Entry entry : repository.entrySet()) {
+            list.add((Post) entry.getValue());
+        }
+        return list;
+    }
+
+    public Optional<Post> getById(long id) {
+        if(repository.get(id) != null) {
+            return Optional.of(repository.get(id));
+        }
+        return Optional.empty();
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            long i = counter.getAndIncrement();
+            while (repository.containsKey(i)) {
+                i = counter.getAndIncrement();
+            }
+            post.setId(i);
+            repository.put(i, post);
+        } else {
+            repository.put(post.getId(), post);
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        repository.remove(id);
+    }
+}
